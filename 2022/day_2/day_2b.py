ebe4138b62b319f65094f814daf7ffb0e3a9d24d
roopeak <roopeak@protonmail.com>
2022-12-02 11:01:06 +0200

    Done.

---
+++ 2022/day_2/day_2b.py
@@ -0,0 +1,45 @@
+
+
+
+def read_file(file):
+
+    your_score = 0
+
+    for line in file:
+
+        # Rock
+        if line[0] == "A":
+            if line[2] == "Y":
+                your_score += 4
+            elif line[2] == "X":
+                your_score += 3
+            elif line[2] == "Z":
+                your_score += 8
+
+        # Paper
+        if line[0] == "B":
+            if line[2] == "X":
+                your_score += 1
+            elif line[2] == "Y":
+                your_score += 5
+            elif line[2] == "Z":
+                your_score += 9
+
+        # Scissors
+        if line[0] == "C":
+            if line[2] == "X":
+                your_score += 2
+            elif line[2] == "Y":
+                your_score += 6
+            elif line[2] == "Z":
+                your_score += 7
+
+    print(your_score)
+
+def main():
+    file = open("input.txt", mode="r")
+    read_file(file)
+
+
+if __name__ == "__main__":
+    main()
